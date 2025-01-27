1febc5ca37d20dde1f07634fabdafb4cd2087d99
Weicheng Xue <sunsincer@qq.com>
2025-01-27 11:21:45 +0800

    Fix some score issues

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -1,6 +1,8 @@
 // Here are some more easy Clippy fixes so you can see its utility 📎
 // TODO: Fix all the Clippy lints.
 
+use std::mem;
+
 #[rustfmt::skip]
 #[allow(unused_variables, unused_assignments)]
 fn main() {
@@ -21,7 +23,7 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    // Use `mem::swap` to correctly swap two values.
+    mem::swap(&mut value_a, &mut value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
